3a7a95aa50c392814718b261f4dbc2f62d3c87a5
hakobvardanyan <hakvardanyan@gmail.com>
2020-04-02 13:44:18 +0400

    Injecting ViewModel with koin.

--- app/src/main/java/com/example/coroutinesflow_koin_mvvm/SingleActivity.kt
+++ app/src/main/java/com/example/coroutinesflow_koin_mvvm/SingleActivity.kt
@@ -1,26 +1,26 @@
 package com.example.coroutinesflow_koin_mvvm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.domain.employee.LoadEmployeeUseCase
-import com.example.domain.repository.EmployeeRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.get
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SingleActivity : AppCompatActivity() {
 
-    val useCase = get<LoadEmployeeUseCase>()
+    val model by viewModel<EmployeesListViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_single)
 
         GlobalScope.launch(Dispatchers.Main) {
-            useCase.execute().collect {
+            model.observeEmployers().collect {
                 Toast.makeText(this@SingleActivity, it.toString(), Toast.LENGTH_LONG).show()
             }
         }
--- app/src/main/java/com/example/coroutinesflow_koin_mvvm/di/ViewModelModule.kt
+++ app/src/main/java/com/example/coroutinesflow_koin_mvvm/di/ViewModelModule.kt
@@ -1,28 +1,11 @@
 package com.example.coroutinesflow_koin_mvvm.di
 
+import com.example.coroutinesflow_koin_mvvm.EmployeesListViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val VIEW_MODEL_MODULE = module {
 
-    //application
-//    factory { AppPresenter(get(), get(), get(), get(), get(), get(), get(), get(), get()) }
-//
-//
-//    //activities
-//    factory<HomeScreenContract.Presenter> { HomeScreenPresenter(get(), get(), get(), get(), get(),
-//            get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(),
-//            get(), get(), get(), get(named<HomeRouter>()), get(), get()) }
-//
-//    factory<SplashScreenContract.Presenter> { SplashScreenPresenter(get(), get()) }
-//
-//    factory<MainContract.Presenter> { MainPresenter(get(named<AppFragmentRouter>())) }
-//
-//
-//    //fragments
-//    factory<MenuContainerContract.Presenter> { MenuContainerPresenter(get()) }
-//
-//    factory<PAFeatureContainerContract.Presenter> { PAFeatureContainerPresenter(get(named<PAFeatureRouter>())) }
-//
-//    factory<MagazineContract.Presenter> { MagazinePresenter(get(), get(), get(), get(), get(), get(), get(), get()) }
+    viewModel { EmployeesListViewModel(get()) }
 
 }
